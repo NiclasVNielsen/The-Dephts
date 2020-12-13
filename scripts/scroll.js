fbeb1d417369b2ffdeb3b509edf8f96eebb6b942
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2020-12-13 22:45:24 +0100

    Fade in/out animation

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,12 +3,15 @@ window.addEventListener('resize', function () {
     height = document.documentElement.clientHeight;
 });
 
-function fadeChanger (element, option){
-    const classes = ['fadeoutdelayed', 'fadeindelayed', 'fadeout', 'fadein'];
+const html = document.querySelector('html');
+const imagegalleryfade = [document.querySelector('#image-gallery > .fade'), document.querySelector('#info-page-1')];
+let boble = document.querySelectorAll('.bobble');
 
-    classes.forEach(classe => {
+function fadeChanger (element, option){
+
+    /* classes.forEach(classe => {
         element.classList.remove(classe);
-    });
+    }); */
 
     if(option == 3){
         element.classList.add('fadeindelayed');
@@ -21,9 +24,42 @@ function fadeChanger (element, option){
     }
 }
 
-const html = document.querySelector('html');
-const imagegalleryfade = [document.querySelector('#image-gallery > .fade'), document.querySelector('#info-page-1')];
-let boble = document.querySelectorAll('.bobble');
+const sections = [
+    [document.querySelector('#front-page h1'), document.querySelector('#front-page p')],
+    [document.querySelector('#info-page-1 h3'), document.querySelector('#info-page-1 p')],
+    [document.querySelector('#image-gallery h4'), document.querySelector('#image-gallery p')],
+    [document.querySelector('#info-page-2 h4'), document.querySelector('#info-page-2 p')],
+    [document.querySelector('#info-page-3 h4'), document.querySelector('#info-page-3 p'), document.querySelector('#info-page-3 p:last-of-type')],
+    [document.querySelector('#info-page-4 h4'), document.querySelector('#info-page-4 p')]
+];
+
+function erase(selected) {
+    const cssFadeInClasses = ['fadeindelayed', 'fadein'];
+    const cssFadeoutClasses = ['fadeoutdelayed', 'fadeout'];
+    sections.forEach((section, index) => {
+        if(selected != index){
+            section.forEach(text => {
+                cssFadeInClasses.forEach(cssClass => {
+                    text.classList.remove(`${cssClass}`);
+                })
+                text.classList.add('fadeout');
+            });
+        } else {
+            section.forEach((text, secondIndex) => {
+                cssFadeoutClasses.forEach(cssClass => {
+                    text.classList.remove(`${cssClass}`);
+                })
+                if(secondIndex == 0){
+                    text.classList.add('fadein');
+                }else{
+                    text.classList.add('fadeindelayed');
+                }
+            });
+        }
+    });
+}
+
+let test = 0;
 
 /* Scroll Style Switch --------------------------------------------- */
 document.addEventListener('scroll', function () {
@@ -34,7 +70,43 @@ document.addEventListener('scroll', function () {
         0 fade out    
     */
 
-
+    if(html.scrollTop < height){
+        if(test != 1){
+            test = 1;
+            erase(0);
+            //console.log(test);
+        }
+    }else if(html.scrollTop >= height && html.scrollTop < height * 2){
+        if(test != 2){
+            test = 2;
+            erase(1);
+            //console.log(test);
+        }
+    }else if(html.scrollTop >= height * 2 && html.scrollTop < height * 3){
+        if(test != 3){
+            test = 3;
+            erase(2);
+            //console.log(test);
+        }
+    }else if(html.scrollTop >= height * 3 && html.scrollTop < height * 4){
+        if(test != 4){
+            test = 4;
+            erase(3);
+            //console.log(test);
+        }
+    }else if(html.scrollTop >= height * 4 && html.scrollTop < height * 5){
+        if(test != 5){
+            test = 5;
+            erase(4);
+            //console.log(test);
+        }
+    }else if(html.scrollTop >= height * 5 && html.scrollTop < height * 6){
+        if(test != 6){
+            test = 6;
+            erase(5);
+            //console.log(test);
+        }
+    }
 
 
     /* Bobler ---------------------------------------------------------- */
